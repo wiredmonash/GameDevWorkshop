cbd821912d213c43ec25aed5d2d56126ebe7bec5
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2024-04-20 13:52:55 +1000

    Inital commit with base game functionality
    Additions:
    - Added player scene with sprite, collision and script attached.
    - Added player script with base functionality such as: movement left and right (A and D keys), jumping (SPACEBAR key), force of gravity down, and sprite flipping based on movement direction (left or right).
    - Player can collide with StaticBody2D objects because the Player is a CharacterBody2D with a Collision2D child.
    - Added Camera2D which is a child of the Player, therefore the Camera2D will follow the player smoothly with a speed of 5px/s. The camera is also zoomed at 4x.
    - Added parallax background that has a parallax speed of 0.2 (this makes it seem to appear that the background is further away).
    - Added wall scene which is a StaticBody2D with a Collision2D child, this is what the player will collide with.
    
    Notes:
    Currently researching the true functionality of the move_and_slide() built-in function, to be better at explaining its meaning.

---
+++ player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+const GRAVITY = 980.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -350.0
+
+func _physics_process(delta):
+	# A force of gravity on the player
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	
+	# Jumping
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# Movement 
+	var dir = Input.get_axis('left', 'right')
+	if dir:
+		# Slowing down base on the input	
+		velocity.x = dir * SPEED
+	else:
+		# Slowing down if there is no input
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Sprite flipping
+	if Input.is_action_just_pressed('right'): $Sprite2D.flip_h = false
+	if Input.is_action_just_pressed('left'): $Sprite2D.flip_h = true
+	
+	# Apply velocity
+	move_and_slide()
