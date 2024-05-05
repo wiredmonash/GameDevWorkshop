4e7355804f8f0610b3737080220353bf20145309
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2024-05-05 11:19:46 +1000

    Switched project to Compatibility mode

--- player.gd
+++ player.gd
@@ -12,11 +12,11 @@ func _physics_process(delta):
 	# Jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
+	
 	# Movement 
 	var dir = Input.get_axis('left', 'right')
 	if dir:
-		# Slowing down base on the input	
+		# Slowing down base on the input
 		velocity.x = dir * SPEED
 	else:
 		# Slowing down if there is no input
